1a63179cafe9a8cfc17c9d2417ca07de8cd4e0e9
Leandro Silva <lsilva.info@gmail.com>
2024-10-30 09:41:37 -0300

    Initial commit

---
+++ src/main/java/br/dev/leandro/spring/cloud/user/SpringUserApplication.java
@@ -0,0 +1,13 @@
+package br.dev.leandro.spring.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/leandro/spring/cloud/user/SpringUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.leandro.spring.cloud.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
